74994f933901606afbee6856605e75f93826ebb5
Piotr Truszkowski <truszkowski@gmail.com>
2024-10-14 10:10:23 +0200

    apikey added

--- worker_pool.tf
+++ worker_pool.tf
@@ -6,18 +6,22 @@ module "worker_pool" {
     export SPACELIFT_POOL_PRIVATE_KEY="${base64encode(tls_private_key.main.private_key_pem)}"
   EOT
 
-  max_size              = "${var.min_size}"
-  min_size              = "${var.max_size}"
-  ec2_instance_type     = "${var.ec2_instance_type}"
-  security_groups       = [aws_security_group.main.id]
-  vpc_subnets           = module.vpc.private_subnets
-  worker_pool_id        = spacelift_worker_pool.aws.id
+  max_size          = var.min_size
+  min_size          = var.max_size
+  ec2_instance_type = var.ec2_instance_type
+  security_groups   = [aws_security_group.main.id]
+  vpc_subnets       = module.vpc.private_subnets
+  worker_pool_id    = spacelift_worker_pool.aws.id
+  
   enable_autoscaling    = true
   autoscaler_s3_package = {
     bucket = aws_s3_bucket.autoscaler_binary.id
     key    = aws_s3_object.autoscaler_binary.id
   }
 
+  spacelift_api_key_id       = var.spacelift_api_key_id
+  spacelift_api_key_secret   = var.spacelift_api_key_secret
+  spacelift_api_key_endpoint = var.spacelift_api_key_endpoint
 }
 
 resource "spacelift_worker_pool" "aws" {
