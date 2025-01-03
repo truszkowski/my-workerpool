c4ceab6334927ae8e5721e0020e454b3dae96c30
Piotr Truszkowski <truszkowski@gmail.com>
2025-01-03 23:14:33 +0100

    Update s3_autoscaler.tf

--- s3_autoscaler.tf
+++ s3_autoscaler.tf
@@ -4,17 +4,17 @@
 # source and aws_s3_object resource in this manner. Instead, use an external process
 # to host the binary in your own S3 bucket.
 
-resource "aws_s3_bucket" "autoscaler_binary" {
-  bucket_prefix = "my-workerpool-autoscaler"
-}
+#resource "aws_s3_bucket" "autoscaler_binary" {
+#  bucket_prefix = "my-workerpool-autoscaler"
+#}
 
-data "http" "autoscaler_binary" {
-  url = "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
-}
+#data "http" "autoscaler_binary" {
+#  url = "https://github.com/spacelift-io/ec2-workerpool-autoscaler/releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
+#}
 
-resource "aws_s3_object" "autoscaler_binary" {
-  key            = "releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
-  bucket         = aws_s3_bucket.autoscaler_binary.id
-  content_base64 = data.http.autoscaler_binary.response_body_base64
-  content_type   = "application/octet-stream"
-}
+#resource "aws_s3_object" "autoscaler_binary" {
+#  key            = "releases/download/${var.autoscaler_version}/ec2-workerpool-autoscaler_linux_${var.autoscaler_architecture}.zip"
+#  bucket         = aws_s3_bucket.autoscaler_binary.id
+#  content_base64 = data.http.autoscaler_binary.response_body_base64
+#  content_type   = "application/octet-stream"
+#}
